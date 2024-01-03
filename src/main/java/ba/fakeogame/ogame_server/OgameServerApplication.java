3bcd2437a0e7d276ea28b0b8e003437420d74111
Igor Pavlović <pigorp@hotmail.com>
2024-01-03 19:12:40 +0100

    Cuvanje univerzuma u fajl i ucitavanje, mutex za univerzum
    Novi endpointi:
    updateBuilding
    updateBuildingInfo
    updateShip
    buildShipInfo

--- src/main/java/ba/fakeogame/ogame_server/OgameServerApplication.java
+++ src/main/java/ba/fakeogame/ogame_server/OgameServerApplication.java
@@ -27,38 +27,31 @@ public class OgameServerApplication {
 		SpringApplication.run(OgameServerApplication.class, args);
 	}
 	
-	@GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name,
-    					@RequestParam(value = "surname", defaultValue = "World") String surname) {
-		JSONObject json = new JSONObject();
-		
-		Ship ship = ShipIndex.getInstance().getById("scout");
-		json.put("name", ship.getName());
-		json.put("health", ship.getHealth());
-		json.put("speed", ship.getSpeed());
-		return json.toString();
-	
-    }
-	
 	@CrossOrigin(origins = "http://127.0.0.1:5500")
 	@GetMapping("/register")
 	public String register(String username, String password)
 	{
-		Player p = UniverseProvider.getUniverse().register(username, password);
-		JSONObject json = new JSONObject();
-		
-		if(p != null)
+		synchronized(UniverseProvider.mutex)
 		{
-			json.put("success", true);
-			System.out.println("Igrac " + p.getName() + " je registrovan!");
-		}
-		else
-		{
-			json.put("success", false);
-			System.out.println("Registracija nije uspijesna! ");
-		}
+			
+			Player p = UniverseProvider.getUniverse().register(username, password);
+			JSONObject json = new JSONObject();
+			
+			if(p != null)
+			{
+				json.put("success", true);
+				System.out.println("Igrac " + p.getName() + " je registrovan!");
+				UniverseProvider.save();
+			}
+			else
+			{
+				json.put("success", false);
+				System.out.println("Registracija nije uspijesna! ");
+			}
+			
+			return json.toString();
 		
-		return json.toString();
+		}
 		
 	}
 	
@@ -66,45 +59,46 @@ public class OgameServerApplication {
 	@GetMapping("/login")
 	public String login(String username, String password)
 	{
-		System.out.println("STIGAO LOGIN: " + username + " / " + password);
-		
-		Player p = UniverseProvider.getUniverse().login(username, password);
-		
-		JSONObject json = new JSONObject();
-		
-		if(p != null)
+		synchronized(UniverseProvider.mutex)
 		{
-			json.put("success", true);
-			json.put("username", p.getName());
-			json.put("ID", p.getId());
-			json.put("token", p.getToken());
+			System.out.println("STIGAO LOGIN: " + username + " / " + password);
 			
-			JSONArray planetArray = new JSONArray();
-			JSONArray buildingsArray = new JSONArray();
+			Player p = UniverseProvider.getUniverse().login(username, password);
 			
-			for(Planet planet : p.getOwnedPlanets())
+			JSONObject json = new JSONObject();
+			
+			if(p != null)
 			{
-				planetArray.put(planet.getId());
+				json.put("success", true);
+				json.put("username", p.getName());
+				json.put("ID", p.getId());
+				json.put("token", p.getToken());
 				
-				for(Buidling b : )
+				JSONArray planetArray = new JSONArray();
+				
+				for(Planet planet : p.getOwnedPlanets())
+				{
+					planetArray.put(planet.getId());
+				}
+				
+				
+				json.put("ownedPlanets", planetArray);
+				
+				
+				System.out.println(p.getName() + " " + p.getToken());
+				System.out.println(username + " " + p.getOwnedPlanets().size());
+				
+				System.out.println("ODGOVOR:\n" + json);
+				UniverseProvider.save();
 			}
-			json.put("ownedPlanets", planetArray);
+			else
+			{
+				json.put("success", false);
+			}
 			
-			
-			
-			
-			
-			System.out.println(p.getName() + " " + p.getToken());
-			System.out.println(username + " " + p.getOwnedPlanets().size());
-			
-			
-		}
-		else
-		{
-			json.put("success", false);
-		}
+			return json.toString();
 		
-		return json.toString();
+		}
 		
 	}
 	
@@ -112,22 +106,26 @@ public class OgameServerApplication {
 	@GetMapping("/signOut")
 	public String signOut(String token)
 	{
-		Player p = UniverseProvider.getUniverse().authenticate(token);
-		
-		JSONObject json = new JSONObject();
-		
-		if(p != null)
+		synchronized(UniverseProvider.mutex)
 		{
-			json.put("success", true);
-			p.clearToken();
-			System.out.println("Token je obrisan! Izlogovani igrac je: " + p.getName());
+			Player p = UniverseProvider.getUniverse().authenticate(token);
+			
+			JSONObject json = new JSONObject();
+			
+			if(p != null)
+			{
+				json.put("success", true);
+				p.clearToken();
+				System.out.println("Token je obrisan! Izlogovani igrac je: " + p.getName());
+				UniverseProvider.save();
+			}
+			else
+			{
+				json.put("success", false);
+			}
+			
+			return json.toString();
 		}
-		else
-		{
-			json.put("success", false);
-		}
-		
-		return json.toString();
 	}
 	
 	
@@ -135,28 +133,31 @@ public class OgameServerApplication {
 	@GetMapping("/resource")
 	public String resource(String token, int idPlanet)
 	{
-		Player p = UniverseProvider.getUniverse().authenticate(token);
-		Planet planet = UniverseProvider.getUniverse().getPlanet(idPlanet);
-		
-		JSONObject json = new JSONObject();
-		
-		if(p != null && planet != null && p == planet.getOwner())
+		synchronized(UniverseProvider.mutex)
 		{
-			ResourceAmount r = planet.getResource();
+			Player p = UniverseProvider.getUniverse().authenticate(token);
+			Planet planet = UniverseProvider.getUniverse().getPlanet(idPlanet);
 			
-			json.put("success", true);
-			json.put("iron", r.getSingleResource(ResourceType.IRON));
-			json.put("crystal", r.getSingleResource(ResourceType.CRYSTAL));
-			json.put("deuterium", r.getSingleResource(ResourceType.DEUTERIUM));
-			json.put("energyProduction", planet.getEnergyProduction());
-			json.put("energyConsumption", planet.getEnergyConsumption());
+			JSONObject json = new JSONObject();
+			
+			if(p != null && planet != null && p == planet.getOwner())
+			{
+				ResourceAmount r = planet.getResource();
+				
+				json.put("success", true);
+				json.put("iron", r.getSingleResource(ResourceType.IRON));
+				json.put("crystal", r.getSingleResource(ResourceType.CRYSTAL));
+				json.put("deuterium", r.getSingleResource(ResourceType.DEUTERIUM));
+				json.put("energyProduction", planet.getEnergyProduction());
+				json.put("energyConsumption", planet.getEnergyConsumption());
+			}
+			else
+			{
+				json.put("success", false);
+			}
+			
+			return json.toString();
 		}
-		else
-		{
-			json.put("success", false);
-		}
-		
-		return json.toString();
 		
 	}
 	
@@ -164,70 +165,209 @@ public class OgameServerApplication {
 	@GetMapping("/planets")
 	public String planets(String token)
 	{
-		Player p = UniverseProvider.getUniverse().authenticate(token);
-		JSONObject json = new JSONObject();
-		
-		if(p != null)
+		synchronized(UniverseProvider.mutex)
 		{
-			JSONArray planetsArray = new JSONArray();
+			Player p = UniverseProvider.getUniverse().authenticate(token);
+			JSONObject json = new JSONObject();
 			
-			json.put("success", true);
-			
-			for(Planet planets : p.getOwnedPlanets())
+			if(p != null)
 			{
-				JSONObject planetJSON = new JSONObject();
+				JSONArray planetsArray = new JSONArray();
 				
-				planetJSON.put("id",planets.getId());
-				planetJSON.put("name", planets.getName());
-				planetJSON.put("positionX", planets.getPositionX());
-				planetJSON.put("positionY", planets.getPositionY());
+				json.put("success", true);
 				
-				planetsArray.put(planetJSON);
+				for(Planet planets : p.getOwnedPlanets())
+				{
+					JSONObject planetJSON = new JSONObject();
+					
+					planetJSON.put("id",planets.getId());
+					planetJSON.put("name", planets.getName());
+					planetJSON.put("positionX", planets.getPositionX());
+					planetJSON.put("positionY", planets.getPositionY());
+					
+					planetsArray.put(planetJSON);
+				}
+				json.put("ownedPlanets", planetsArray);
 			}
-			json.put("ownedPlanets", planetsArray);
+			else
+			{
+				json.put("success", false);
+			}
+			
+			return json.toString();
 		}
-		else
-		{
-			json.put("success", false);
-		}
-		
-		return json.toString();
 	}
 	
 	@CrossOrigin(origins = "http://127.0.0.1:5500")
 	@GetMapping("/updateBuilding")
 	public String updateBuilding(String token, int idPlanet, String idBuilding)
 	{
-		Player p = UniverseProvider.getUniverse().authenticate(token);
-		Planet planet = UniverseProvider.getUniverse().getPlanet(idPlanet);
-		Building building = BuildingIndex.getInstance().getById(idBuilding);
-		
-		JSONObject json = new JSONObject();
-		
-		if(p != null && planet != null && p == planet.getOwner() && building != null)
+		synchronized(UniverseProvider.mutex)
 		{
+			Player p = UniverseProvider.getUniverse().authenticate(token);
+			Planet planet = UniverseProvider.getUniverse().getPlanet(idPlanet);
+			Building building = BuildingIndex.getInstance().getById(idBuilding);
 			
-			ConstructionResult result = planet.constructBuilding(building);
+			JSONObject json = new JSONObject();
 			
-			json.put("success", result == ConstructionResult.OK);
-			json.put("name", building.getName());
-			json.put("conTime", building.getConstractionTimer());
-			json.put("status", result);
-			json.put("lvl", planet.getBuildingLvl(building));
+			if(p != null && planet != null && p == planet.getOwner() && building != null)
+			{
+				
+				ConstructionResult result = planet.constructBuilding(building);
+				
+				json.put("success", result == ConstructionResult.OK);
+				json.put("name", building.getName());
+				json.put("conTime", building.getConstractionTimer());
+				json.put("status", result);
+				json.put("lvl", planet.getBuildingLvl(building));
+				
+				System.out.println("Status gradnje zgrade je " + result);
+				
+				if(result == ConstructionResult.OK)
+				{
+					UniverseProvider.save();
+				}
+				
+			}
+			else
+			{
+				json.put("success", false);
+				
+				System.out.println("Zgrada neuspjesno unapredjena! ");
+			}
 			
-			System.out.println("Status gradnje je " + result);
-			
+			return json.toString();
 		}
-		else
-		{
-			json.put("success", false);
-			
-			System.out.println("Zgrada neuspjesno unapredjena! ");
-		}
-		
-		return json.toString();
 		
 	}
+	
+	@CrossOrigin(origins = "http://127.0.0.1:5500")
+	@GetMapping("/buildShip")
+	public String updateShip(String token, int idPlanet, String idShip, int count)
+	{
+		synchronized(UniverseProvider.mutex)
+		{
+			Player p = UniverseProvider.getUniverse().authenticate(token);
+			Planet planet = UniverseProvider.getUniverse().getPlanet(idPlanet);
+			Ship ship = ShipIndex.getInstance().getById(idShip);
+			
+			
+			JSONObject json = new JSONObject();
+			
+			if(p != null && planet != null && p == planet.getOwner() && ship != null && count > 0)
+			{
+				ConstructionResult result = planet.constructShip(ship, count);
+				
+				json.put("success", result == ConstructionResult.OK);
+				json.put("name", ship.getName());
+				json.put("count", count);
+				json.put("conTime", ship.getConstructionTimer());
+				json.put("totalConTime", ship.getConstructionTimer() * count);
+				json.put("status", result);
+				
+				System.out.println("Status gradenje brodova je: " + result);
+				
+				if(result == ConstructionResult.OK)
+				{
+					UniverseProvider.save();
+				}
+			}
+			else
+			{
+				json.put("success", false);
+				
+				System.out.println("Brodovi nisu poceli da se prave! ");
+			}
+			
+			return json.toString();
+		}
+	}
+	
+	@CrossOrigin(origins = "http://127.0.0.1:5500")
+	@GetMapping("/updateBuildingInfo")
+	public String updateBuildingInfo(String token, int idPlanet)
+	{
+		synchronized(UniverseProvider.mutex)
+		{
+			Player p = UniverseProvider.getUniverse().authenticate(token);
+			Planet planet = UniverseProvider.getUniverse().getPlanet(idPlanet);
+	
+			
+			JSONObject json = new JSONObject();
+			
+			if(p != null && planet != null && p == planet.getOwner())
+			{
+				Building b = planet.getBuildingInConstruction();
+				
+				json.put("success", true);
+				
+				if(b != null)
+				{
+					json.put("isConstructingBuilding", true);
+					json.put("id", b.getId());
+					json.put("name", b.getName());
+					json.put("buildingLvl", planet.getBuildingLvl(b));
+					json.put("conTime", planet.getBuildingConstructionTimer());
+				}
+				else
+				{
+					json.put("isConstructingBuilding", false);
+				}
+			}
+			else
+			{
+				json.put("successs", false);
+				System.out.println("Greska u upitu. ");
+			}
+			
+			return json.toString();
+		}
+		
+	}
+	
+	@CrossOrigin(origins = "http://127.0.0.1:5500")
+	@GetMapping("/buildShipInfo")
+	public String buildShipInfo(String token, int idPlanet)
+	{
+		synchronized(UniverseProvider.mutex)
+		{
+			Player p = UniverseProvider.getUniverse().authenticate(token);
+			Planet planet = UniverseProvider.getUniverse().getPlanet(idPlanet);
+			
+			JSONObject json = new JSONObject();
+			
+			if(p != null && planet != null && p == planet.getOwner())
+			{
+				Ship s = planet.getShipInConstruction();
+				
+				json.put("success", true);
+				
+				if(s != null)
+				{
+					json.put("isConstructionShip", true);
+					json.put("id", s.getId());
+					json.put("name", s.getName());
+					json.put("countShips", planet.getConShipCount());
+					json.put("conTime", planet.getShipConstructionTimer());
+					json.put("fullConTime", planet.getfullConShipTimer());
+				}
+				else
+				{
+					json.put("isConstructionShip", false);
+				}
+			}
+			else
+			{
+				json.put("success", false);
+				System.out.println("Greska u upitu. ");
+			}
+			
+			return json.toString();
+		}
+		
+		
+	}
+	
 	
 	/*Rijesiti resurse u endpointu resource da ne spominjem nazive resorsa po imenu,
 	  napraviti da bude fleksibilno za dalje tok igre ako dodje do promjena.*/
--- src/main/java/ba/fakeogame/ogame_server/UniverseProvider.java
+++ src/main/java/ba/fakeogame/ogame_server/UniverseProvider.java
@@ -1,22 +1,47 @@
 package ba.fakeogame.ogame_server;
 
+import java.io.File;
+
+import pak.Planet;
 import pak.Universe;
 
 public class UniverseProvider
 {
 	private static Universe universe = null;
+	public static Object mutex = new Object();
 	
 	public static Universe getUniverse()
 	{
 		if(universe == null)
 		{
-			universe = new Universe(100, 100, 100);
-			universe.register("Igor", "123456");
-			universe.register("Aco", "654321");
+			File universeFile = new File("Universe.json");
+			
+			if(universeFile.exists() && !universeFile.isDirectory())
+			{
+				System.out.println("Ucitavamo univerzum iz fajla.");
+				universe = new Universe("Universe.json");
+			}
+			else
+			{
+				System.out.println("Nema fajla, pravimo novi univerzum.");
+				
+				universe = new Universe(100, 100, 100);
+				universe.register("Igor", "123456");
+				universe.register("Aco", "654321");
+				Planet planet1 = universe.getPlanet(25);
+				universe.getPlayer(1).takePlanet(planet1);
+				
+				universe.saveToFile("Universe.json");
+			}
 		}
 		
 		universe.update();
 		
 		return universe;
+	}
+	
+	public static void save()
+	{
+		getUniverse().saveToFile("Universe.json");
 	}
 }
